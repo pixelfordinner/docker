b88990b1f41b8e2bd5f7dae39e09177ae22f351c
Karl Fathi <karl.fathi@gmail.com>
2016-09-14 14:41:50 -0300

    - Change: PHP image now uses alpine base - Tweak: Opcache and APCu have decreased memory - Change: PHP image can now take a username/uid, groupname/gid and umask as env variables

--- upstream/php/data/composer.sh
+++ upstream/php/data/composer.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-# This is a wrapper so that wp-cli can run as the www-data user
-sudo -E -u www-data /usr/local/bin/composer.phar -d=/opt/www $*
+
+USER_NAME=${LOCAL_USER_NAME:-'www-data'}
+GROUP_NAME=${LOCAL_GROUP_NAME:-'www-data'}
+
+# This is a wrapper so that composer can run as the local user or as www-data user (Alpine default)
+su-exec $USER_NAME:$GROUP_NAME /usr/local/bin/composer.phar -d=/opt/www $*
